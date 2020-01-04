568297faaf32e621ea4987f97aaccdc204db2842
Dilruba Khan <rubatanisa@yahoo.ie>
2020-01-04 15:46:53 +0000

    Implement MVC

---
+++ JavaDesignPattern/src/app/Application.java
@@ -0,0 +1,28 @@
+package app;
+import javax.swing.SwingUtilities;
+
+import controller.Controller;
+import model.Model;
+import view.View;
+
+public class Application {
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				runApp();
+			}
+			
+		});
+	}
+	
+	public static void runApp() {
+		Model model = new Model();
+		View view = new View(model);
+
+		Controller controller = new Controller(view, model);
+	}
+
+}
